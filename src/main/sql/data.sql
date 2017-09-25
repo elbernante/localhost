a705c5c019c83c4f24ba45f26325556cdd25eec4
Peter James Bernante <elbernante@gmail.com>
2017-09-25 12:18:31 -0500

    Add dummy users

--- src/main/sql/data.sql
+++ src/main/sql/data.sql
@@ -1,6 +1,18 @@
 insert into SampleModel (name) values ('Sample Model');
 
--- Users
+-- Dummy Users
 
 insert into Credentials (id, username, password, enabled) values (1, 'user1', '$2a$04$onojKV4YJ8/T7v9eRzwM7.Qk0kv2rDztIN9odhY/ozm1PMmzUS9rG', true);
-insert into Authority (id, user_id, authority) values (2, 1 , 'USER_ROLE');
+insert into Authority (id, user_id, authority) values (2, 1, 'USER_ROLE');
+
+insert into Credentials (id, username, password, enabled) values (3, 'meron', '$2a$04$onojKV4YJ8/T7v9eRzwM7.Qk0kv2rDztIN9odhY/ozm1PMmzUS9rG', true);
+insert into Authority (id, user_id, authority) values (4, 3, 'USER_ROLE');
+
+insert into Credentials (id, username, password, enabled) values (5, 'rafael', '$2a$04$onojKV4YJ8/T7v9eRzwM7.Qk0kv2rDztIN9odhY/ozm1PMmzUS9rG', true);
+insert into Authority (id, user_id, authority) values (6, 5, 'USER_ROLE');
+
+insert into Credentials (id, username, password, enabled) values (7, 'yang', '$2a$04$onojKV4YJ8/T7v9eRzwM7.Qk0kv2rDztIN9odhY/ozm1PMmzUS9rG', true);
+insert into Authority (id, user_id, authority) values (8, 7, 'USER_ROLE');
+
+insert into Credentials (id, username, password, enabled) values (9, 'peter', '$2a$04$onojKV4YJ8/T7v9eRzwM7.Qk0kv2rDztIN9odhY/ozm1PMmzUS9rG', true);
+insert into Authority (id, user_id, authority) values (10, 9, 'USER_ROLE');
